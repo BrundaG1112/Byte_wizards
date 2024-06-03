34f4fcd7b64a0bf70c777fffd1c3bf7a4d2fbff8
BrundaG1112 <139426767+BrundaG1112@users.noreply.github.com>
2024-06-03 19:44:09 +0530

    Create app.py

---
+++ Prototype_4/app.py
@@ -0,0 +1,136 @@
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template,session,flash,redirect, url_for, session,flash
+from werkzeug.utils import secure_filename
+#from gevent.pywsgi import WSGIServer
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re# Define a flask app
+import cv2
+import math
+import argparse
+import os
+
+app = Flask(__name__)
+app.secret_key = '1a2b3c4d5e'
+
+
+def highest_number(string):
+    numbers = [int(num) for num in string.split() if num.isdigit()]
+    if numbers:
+        return max(numbers)
+    else:
+        return None
+
+def check_ratio(num1, num2):
+    try:
+        if num1 < num2:
+            num1, num2 = num2, num1
+        if num1 / num2 == 3:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def highlightFace(net, frame, conf_threshold=0.7):
+    frameOpencvDnn=frame.copy()
+    frameHeight=frameOpencvDnn.shape[0]
+    frameWidth=frameOpencvDnn.shape[1]
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections=net.forward()
+    faceBoxes=[]
+    for i in range(detections.shape[2]):
+        confidence=detections[0,0,i,2]
+        if confidence>conf_threshold:
+            x1=int(detections[0,0,i,3]*frameWidth)
+            y1=int(detections[0,0,i,4]*frameHeight)
+            x2=int(detections[0,0,i,5]*frameWidth)
+            y2=int(detections[0,0,i,6]*frameHeight)
+            faceBoxes.append([x1,y1,x2,y2])
+            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn,faceBoxes
+
+
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        faceProto="opencv_face_detector.pbtxt"
+        faceModel="opencv_face_detector_uint8.pb"
+        ageProto="age_deploy.prototxt"
+        ageModel="age_net.caffemodel"
+        genderProto="gender_deploy.prototxt"
+        genderModel="gender_net.caffemodel"
+
+        MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+        ageList=['(0 to 2)', '(4 to 6)', '(8 to 12)', '(15 to 20)', '(25 to 32)', '(38 to 43)', '(48 to 53)', '(60 to 100)']
+        genderList=['Male','Female']
+
+        faceNet=cv2.dnn.readNet(faceModel,faceProto)
+        ageNet=cv2.dnn.readNet(ageModel,ageProto)
+        genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+        video=cv2.VideoCapture(file_path)
+        padding=20
+        kidcount=0
+        adultcount=0
+        while cv2.waitKey(1)<0 :
+                hasFrame,frame=video.read()
+                if not hasFrame:
+                        cv2.waitKey()
+                        break
+                resultImg,faceBoxes=highlightFace(faceNet,frame)
+                if not faceBoxes:
+                        print("No face detected")
+                for faceBox in faceBoxes:
+                        face=frame[max(0,faceBox[1]-padding):min(faceBox[3]+padding,frame.shape[0]-1),max(0,faceBox[0]-padding):min(faceBox[2]+padding, frame.shape[1]-1)]
+                        blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+                        genderNet.setInput(blob)
+                        genderPreds=genderNet.forward()
+                        gender=genderList[genderPreds[0].argmax()]
+                        print(f'Gender: {gender}')
+                        ageNet.setInput(blob)
+                        agePreds=ageNet.forward()
+                        age=ageList[agePreds[0].argmax()]
+                        age=f'Age: {age[1:-1]} years'
+                        agel=int(highest_number(age))
+                        if (agel  < 15 ):
+                                kidcount=kidcount+1
+                                print("No of Children: "+str(kidcount))
+                        else:
+                                adultcount=adultcount+1
+                                print("No of Adult : "+str(adultcount))
+                        cv2.putText(resultImg, f' {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
+
+                cv2.imshow("Detecting age and gender", resultImg)
+                cv2.waitKey(0)
+                ts=check_ratio(kidcount,adultcount)
+                if(ts==True or kidcount<adultcount):
+                        return ("Childrens Under Sufficient Monitoring of Adults")
+                else:
+                        return ("Childrens are not Under Sufficient Monitoring of Adults")
+                        
+    return None
+
+
+if __name__ == '__main__':
+    app.run(port=5001,debug=True)
